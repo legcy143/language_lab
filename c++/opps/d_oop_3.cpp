8bf76cae3211cdf7764a72c34f09c7d9cbc824fa
legcy <legcy143@gmail.com>
2023-09-23 21:11:58 +0530

    learning 23092023

--- c++/opps/d_oop_3.cpp
+++ c++/opps/d_oop_3.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 
-class m1{
-    
-}
+class m1
+{
+public:
+    int a = 101;
+};
 
-int main(){
-    std::cout<<"har har mahadev ~  dynamic oop";
+int main()
+{
+    std::cout << "har har mahadev ~  dynamic oop\n";
+    // static allocation of m1 class
+    m1 m1s;
+    std::cout <<"static "<<m1s.a<<"\n";
+
+
+    // dynmic allocation of class m1 class
+    m1 *m1a = new m1();
+    std::cout <<"dynamic "<<m1a->a;
 }
---
+++ c++/roughs/rough_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+int main(){
+    std::cout<<"test\n";
+    int arr1[2] = {1,2};
+    int arr2[2] = {1,2};
+
+    std::cout<<"sum "<<arr1[0]+arr2[0];
+
+}
