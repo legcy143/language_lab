680e31a7313fd83afa3fcc9669327fab81416381
legcy <legcy143@gmail.com>
2024-04-23 12:09:15 +0530

    lengthoflastword leetcode

--- lc-d-75/practice.cpp
+++ lc-d-75/practice.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 #include <unordered_map>
 #include <unordered_set>
+using namespace std;
 
-int lengthOfLongestSubstring(std::string s)
+int lengthOfLastWord(string s)
 {
-    std::unordered_map<char, int> cache={};
+    size_t end = s.find_last_not_of(" \t\n\r\f\v");
+    s = (end == std::string::npos) ? "" : s.substr(0, end + 1);
     int ans = 0;
-    for (int i = 1; i < s.length(); i++)
+    for (int i = s.length() - 1; i >= 0; i--)
     {
-        int curr = s[i];
         
-        if (cache.find(curr) == cache.end())
+        if (s[i] == ' ')
         {
-            ans++;
-            cache[curr] = curr;
+            break;
         }
+        ans++;
     }
+ 
     return ans;
 }
+
 int main()
 {
     std::cout << "har har mahadev \n";
-    std::cout << "\nans " << lengthOfLongestSubstring("abcabcbb"); // 3
-    std::cout << "\nans " << lengthOfLongestSubstring("bbbbb");    // 1
-    std::cout << "\nans " << lengthOfLongestSubstring("pwwkew");   // 3
+    std::cout << "\nans " << lengthOfLastWord("hello world"); // 3
+    std::cout << "\nans " << lengthOfLastWord(" hi lol  ");   // 1
     return 0;
 }
